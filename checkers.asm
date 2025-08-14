7f658f3a1dd45f1d2f75efc3bd92736758113998
Gregory Bowne <gbowne144@gmail.com>
2025-08-14 10:46:52 -0700

    foxcode64 pactch from pastebin

--- checkers.asm
+++ checkers.asm
@@ -23,9 +23,9 @@ start:
     mov ax, 0x4C00
     int 0x21
 
-; ------------------------------
+; -------------------------------
 ; Draw checkerboard (8x8 squares)
-; ------------------------------
+; -------------------------------
 draw_checkerboard:
     mov si, 0           ; Row index (0–7)
 
@@ -37,11 +37,11 @@ draw_checkerboard:
     mov ax, si         ; Move row index to AX
     add ax, di         ; Add column index
     and al, 1          ; Use only the lowest bit to alternate color
-    jz .color_red
-    mov bl, 0x00        ; Black
+    jz .color_blue
+    mov bl, 0x07       ; Greyish White thingy
     jmp .color_set
-.color_red:
-    mov bl, 0x0C        ; Red
+.color_blue:
+    mov bl, 0x01       ; Blue
 .color_set:
 
     ; Compute pixel (x, y) start for current square
@@ -51,10 +51,12 @@ draw_checkerboard:
     mul cx              ; ax = col * 40
     mov dx, ax          ; dx = x
 
+    push dx
     mov ax, si
     mov cx, 25
     mul cx              ; ax = row * 25
     mov cx, ax          ; cx = y
+    pop dx
 
     ; Draw 40x25 square at (dx, cx) with color bl
     push si
@@ -81,41 +83,30 @@ draw_checkerboard:
 ;   bl = color
 ; ----------------------------------------
 draw_square:
-    push ax
-    push bx
-    push cx
-    push dx
-    push si
-    push di
+    pusha
+    mov byte [square_color], bl
+    mov byte [square_width], 40
+    mov byte [square_height], 25
+    mov word [square_xpos], dx
+    mov word [square_ypos], cx
 
-    mov si, cx          ; si = y start
-    mov cx, 25          ; 25 rows
-
+    mov dx, word [square_ypos]
 .row_loop:
-    mov di, dx          ; di = x start
-    mov bx, 40          ; 40 columns
-
+    mov cx, word [square_xpos]
+    mov bl, byte [square_width]
 .col_loop:
     ; Set pixel at (di, si) with color bl
     mov ah, 0x0C
-    mov al, bl
-    mov bh, 0x00        ; page 0
-    int 0x10
-
-    inc di
-    dec bx
+    mov al, byte [square_color]
+    mov bh, 0                   ; page = 0
+    int 0x10                    ; set pixel
+    inc cx
+    dec bl
     jnz .col_loop
-
-    inc si
-    dec cx
+    inc dx
+    dec byte [square_height]
     jnz .row_loop
-
-    pop di
-    pop si
-    pop dx
-    pop cx
-    pop bx
-    pop ax
+    popa
     ret
 
 ; ------------------------------
@@ -123,133 +114,136 @@ draw_square:
 ; ------------------------------
 draw_pieces:
     ; Draw red pieces at the top
-    mov si, 0           ; Row index (0–3)
-    mov di, 1           ; Column index (1, 3, 5, 7)
+    lea si, start_red_table
 
 .draw_red_pieces:
+    mov bl, byte [si]   ; Row index from table (0–3 etc..)
+    inc si
+    mov bh, byte [si]   ; Column index from table (1, 3, 5, 7 etc..)
+
     ; Compute pixel (x, y) start for current piece
     ; Each piece is a circle with a radius of 5 pixels
-    mov ax, di
+    movzx ax, bh
     mov cx, 40
     mul cx              ; ax = col * 40 + 20
     add ax, 20
     mov dx, ax          ; dx = x
-
-    mov ax, si
+    push dx
+    movzx ax, bl
     mov cx, 25
     mul cx              ; ax = row * 25 + 12
     add ax, 12
     mov cx, ax          ; cx = y
-
+    pop dx
     ; Draw piece at (dx, cx) with color 0x0C (red)
-    push si
-    push di
+    push bx
     mov bl, 0x0C        ; Red
     call draw_piece
-    pop di
-    pop si
-
+    pop bx
     ; Move to the next piece
-    add di, 2
-    cmp di, 8
-    jl .draw_red_pieces
-
-    ; Move to the next row
     inc si
-    cmp si, 3
-    jl .draw_red_pieces
+    mov al, byte [si]
+    cmp al, 255
+    jne .draw_red_pieces
 
     ; Draw black pieces at the bottom
-    mov si, 5           ; Row index (5–7)
-    mov di, 0           ; Column index (0, 2, 4, 6)
+    lea si, start_black_table
 
 .draw_black_pieces:
+    mov bl, byte [si]   ; Row index from table (5–7 etc..)
+    inc si
+    mov bh, byte [si]   ; Column index from table (0, 2, 4, 6 etc..)
+
     ; Compute pixel (x, y) start for current piece
     ; Each piece is a circle with a radius of 5 pixels
-    mov ax, di
+    movzx ax, bh
     mov cx, 40
     mul cx              ; ax = col * 40 + 20
     add ax, 20
     mov dx, ax          ; dx = x
-
-    mov ax, si
+    push dx
+    movzx ax, bl
     mov cx, 25
     mul cx              ; ax = row * 25 + 12
     add ax, 12
     mov cx, ax          ; cx = y
-
+    pop dx
     ; Draw piece at (dx, cx) with color 0x00 (black)
-    push si
-    push di
+    push bx
     mov bl, 0x00        ; Black
     call draw_piece
-    pop di
-    pop si
-
+    pop bx
     ; Move to the next piece
-    add di, 2
-    cmp di, 8
-    jl .draw_black_pieces
-
-    ; Move to the next row
     inc si
-    cmp si, 8
-    jl .draw_black_pieces
-
+    mov al, byte [si]
+    cmp al, 255
+    jne .draw_black_pieces
     ret
 
-; ----------------------------------------
-; Draw a piece (circle) at (x=dx, y=cx)
+; ----------------------------
+; Draw a piece at (x=dx, y=cx)
 ; Inputs:
 ;   dx = x position
 ;   cx = y position
 ;   bl = color
-; ----------------------------------------
+; ----------------------------
 draw_piece:
-    push ax
-    push bx
-    push cx
-    push dx
-    push si
-    push di
-
-    ; Draw circle with a radius of 5 pixels
-    mov si, -5
-    mov di, -5
-
-.draw_circle:
-    ; Calculate x and y coordinates
-    mov ax, si
-    imul si
-    mov bx, di
-    imul bx
-    add ax, bx
-    cmp ax, 25
-    jg .skip_pixel
-
-    ; Set pixel at (dx + si, cx + di) with color bl
-    mov ah, 0x0C
-    mov al, bl
-    mov bh, 0x00        ; page 0
-    add dx, si
-    add cx, di
+    pusha
+    mov byte [piece_width], 10
+    mov byte [piece_height], 10
+    mov ax, dx
+    mov dx, cx
+    mov cx, ax
+    sub cx, 5
+    sub dx, 5
+    mov word [piece_x], cx
+    mov word [piece_y], dx
+    mov byte [piece_col], bl
+    lea si, piece_graphic
+.draw_check:
+    lodsb
+    cmp al,0
+    jne .draw_checkpix
+    jmp .no_draw_advance
+ .draw_checkpix:
+    mov ah,0xc
+    mov al,byte [piece_col]
+    mov bh,0
     int 0x10
-    sub dx, si
-    sub cx, di
-
-.skip_pixel:
-    inc si
-    cmp si, 6
-    jle .draw_circle_row
-    mov si, -5
-    inc di
-    cmp di, 6
-    jle .draw_circle
-
-    pop di
-    pop si
-    pop dx
-    pop cx
-    pop bx
-    pop ax
+.no_draw_advance:
+    inc cx
+    dec byte [piece_width]
+    jnz .draw_check
+    mov byte [piece_width], 10
+    mov cx,word [piece_x]
+    inc dx
+    dec byte [piece_height]
+    jnz .draw_check
+    popa
     ret
+
+    ; Some data
+square_width:   db 0
+square_height:  db 0
+square_color:   db 0
+square_xpos:    dw 0
+square_ypos:    dw 0
+
+start_red_table:    db 0,1,0,3,0,5,0,7,1,0,1,2,1,4,1,6,2,1,2,3,2,5,2,7,255
+start_black_table:  db 5,0,5,2,5,4,5,6,6,1,6,3,6,5,6,7,7,0,7,2,7,4,7,6,255
+piece_x:    dw 0
+piece_y:    dw 0
+piece_width:    db 0
+piece_height:   db 0
+piece_col:  db 0
+piece_graphic:
+    db 0,0,0,1,1,1,1,0,0,0
+    db 0,0,1,1,1,1,1,1,0,0
+    db 0,1,1,1,1,1,1,1,1,0
+    db 1,1,1,1,1,1,1,1,1,1
+    db 1,1,1,1,0,0,1,1,1,1
+    db 1,1,1,1,0,0,1,1,1,1
+    db 1,1,1,1,1,1,1,1,1,1
+    db 0,1,1,1,1,1,1,1,1,0
+    db 0,0,1,1,1,1,1,1,0,0
+    db 0,0,0,1,1,1,1,0,0,0
